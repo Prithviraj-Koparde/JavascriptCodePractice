8ce7f287560d0c4d6ad66f2d4308a6f4764234d8
Prithviraj-Koparde <prithvirajkoparde@gmail.com>
2025-10-27 12:02:20 +0530

    mouseanimation , event bubbling, event capturing, character-counter

--- EventListner.js
+++ EventListner.js
@@ -63,125 +63,201 @@ window.addEventListener("keydown", function (val) {
 
 // upload file working button
 
-let btn = document.querySelector("#btn")
-let file = document.querySelector("#choose-file")
+function firstUpload() {
+    let btn = document.querySelector("#btn")
+    let file = document.querySelector("#choose-file")
 
-btn.addEventListener("click", () => {
-    file.click()
-})
+    btn.addEventListener("click", () => {
+        file.click()
+    })
 
-file.addEventListener("change", (val) => {
-    btn.textContent = val.target.files[0].name
-    // console.log(val.target.files[0].name)
-})
+    file.addEventListener("change", (val) => {
+        btn.textContent = val.target.files[0].name
+        // console.log(val.target.files[0].name)
+    })
+}
+// firstUpload()
 
 // second upload button
 
-let btn2 = document.querySelector("#btn-2")
-let choose2 = document.querySelector("#choose-file-2")
+function secondUpload() {
+    let btn2 = document.querySelector("#btn-2")
+    let choose2 = document.querySelector("#choose-file-2")
 
-btn2.addEventListener("click", () => {
-    choose2.click()
-})
+    btn2.addEventListener("click", () => {
+        choose2.click()
+    })
 
-choose2.addEventListener("change", (val) => {
-    let file = val.target.files[0]
-    // console.log(val.target.files[0].name)
-    if (file) {
-        btn2.textContent = file.name
-    }
-})
+    choose2.addEventListener("change", (val) => {
+        let file = val.target.files[0]
+        // console.log(val.target.files[0].name)
+        if (file) {
+            btn2.textContent = file.name
+        }
+    })
+}
+// secondUpload()
 
 // making a submit form on submit displays a card
 
-let form = document.querySelector("#form")
-let input = document.querySelectorAll("#form input")        // to select all input tags in form
-let main = document.querySelector("#form-div")
+function firstForm() {
+    let form = document.querySelector("#form")
+    let input = document.querySelectorAll("#form input")        // to select all input tags in form
+    let main = document.querySelector("#form-div")
 
-form.addEventListener("submit", val => {
-    val.preventDefault()                // this is done to stop refresh of page when submit is clicked
-    // console.log(input)
-    // console.log(input[0].value, input[1].value, input[2].value, input[3].value)
+    form.addEventListener("submit", val => {
+        val.preventDefault()                // this is done to stop refresh of page when submit is clicked
+        // console.log(input)
+        // console.log(input[0].value, input[1].value, input[2].value, input[3].value)
 
-    let card = document.createElement("div")
-    card.classList.add("card")
+        let card = document.createElement("div")
+        card.classList.add("card")
 
-    let profile = document.createElement("div")
-    profile.classList.add("profile")
+        let profile = document.createElement("div")
+        profile.classList.add("profile")
 
-    let img = document.createElement("img")
-    img.setAttribute("src", input[0].value)
+        let img = document.createElement("img")
+        img.setAttribute("src", input[0].value)
 
-    let h1 = document.createElement("h1")
-    h1.textContent = input[1].value
+        let h1 = document.createElement("h1")
+        h1.textContent = input[1].value
 
-    let h2 = document.createElement("h2")
-    h2.textContent = input[2].value
+        let h2 = document.createElement("h2")
+        h2.textContent = input[2].value
 
-    let p = document.createElement("p")
-    p.textContent = input[3].value
+        let p = document.createElement("p")
+        p.textContent = input[3].value
 
-    profile.appendChild(img)
-    card.appendChild(profile)
-    card.appendChild(h1)
-    card.appendChild(h2)
-    card.appendChild(p)
+        profile.appendChild(img)
+        card.appendChild(profile)
+        card.appendChild(h1)
+        card.appendChild(h2)
+        card.appendChild(p)
 
-    main.appendChild(card)
+        main.appendChild(card)
 
-    input.forEach(inp => {              // clearing the form after each submit
-        if (inp.type !== "submit") {      // inp ka type "submit" nahi hona chaiye agar nahi hai to if loop chalao
-            inp.value = ""              // inp.value is set to "" that means blank
-        }
+        input.forEach(inp => {              // clearing the form after each submit
+            if (inp.type !== "submit") {      // inp ka type "submit" nahi hona chaiye agar nahi hai to if loop chalao
+                inp.value = ""              // inp.value is set to "" that means blank
+            }
 
+        })
     })
-})
-
+}
+// firstForm()
 
 // making another submit form which displays cards
 
-let form2 = document.querySelector("#form-2")
-let inputs = document.querySelectorAll("#form-2 input")
-let formMain = document.querySelector(".form-div-2")
+function secondForm() {
+    let form2 = document.querySelector("#form-2")
+    let inputs = document.querySelectorAll("#form-2 input")
+    let formMain = document.querySelector(".form-div-2")
 
-form2.addEventListener("submit", val => {
-    val.preventDefault()
+    form2.addEventListener("submit", val => {
+        val.preventDefault()
 
-    // console.log(inputs[0].value,inputs[1].value,inputs[2].value,inputs[3].value,inputs[4].value,inputs[5].value)
+        // console.log(inputs[0].value,inputs[1].value,inputs[2].value,inputs[3].value,inputs[4].value,inputs[5].value)
 
-    let card2 = document.createElement("div")
-    card2.classList.add("card-2")
+        let card2 = document.createElement("div")
+        card2.classList.add("card-2")
 
-    let profile2 = document.createElement("div")
-    profile2.classList.add("profile-2")
+        let profile2 = document.createElement("div")
+        profile2.classList.add("profile-2")
 
-    let img2 = document.createElement("img")
-    img2.setAttribute("src", inputs[0].value)
+        let img2 = document.createElement("img")
+        img2.setAttribute("src", inputs[0].value)
 
-    let name = document.createElement("h1")
-    name.textContent = inputs[1].value
+        let name = document.createElement("h1")
+        name.textContent = inputs[1].value
 
-    let occupation = document.createElement("h2")
-    occupation.textContent = inputs[2].value
+        let occupation = document.createElement("h2")
+        occupation.textContent = inputs[2].value
 
-    let email = document.createElement("h3")
-    email.textContent = inputs[3].value
+        let email = document.createElement("h3")
+        email.textContent = inputs[3].value
 
-    let number = document.createElement("h3")
-    number.textContent = inputs[4].value
+        let number = document.createElement("h3")
+        number.textContent = inputs[4].value
 
-    let p = document.createElement("p")
-    p.textContent = inputs[5].value
+        let p = document.createElement("p")
+        p.textContent = inputs[5].value
 
-    profile2.appendChild(img2)
-    card2.appendChild(profile2)
-    card2.appendChild(name)
-    card2.appendChild(occupation)
-    card2.appendChild(email)
-    card2.appendChild(number)
-    card2.appendChild(p)
+        profile2.appendChild(img2)
+        card2.appendChild(profile2)
+        card2.appendChild(name)
+        card2.appendChild(occupation)
+        card2.appendChild(email)
+        card2.appendChild(number)
+        card2.appendChild(p)
 
-    formMain.appendChild(card2)
+        formMain.appendChild(card2)
+    })
+}
+// secondForm()
+
+
+// ----------------------------------------------------------
+
+// mouseover and mouseleave
+
+let square = document.querySelector("#square")
+
+// mouseenter or mouseover
+square.addEventListener("mouseover", () => {
+    square.style.backgroundColor = "green"
 })
 
 
+// mouseleave or mouseout
+square.addEventListener("mouseleave", () => {
+    square.style.backgroundColor = "red"
+})
+
+
+// --------------------- Bubbling -----------------------
+
+// **Event bubbling - jabh koi element pe eventlistner nahi laga hota to vo apne 
+//  parent element pe eventlistner dhundta hai agar parent pe bhi nai hoga 
+//  to vo html pe dhundta hai aur nai mila to koi event perform nahi hota
+// AND
+// agar element pe evenetlistner hoga tabhi bhi vo parent pe jump karega aur uska
+// eventlistner bhi perform hoga and then jump on its parent element
+
+let ul = document.querySelector("#ul-div ul")
+
+ul.addEventListener("click", val => {
+    // val.target.style.textDecoration = "line-through"
+    val.target.classList.toggle("line-through")         // two way animation of line through with toggle
+})
+
+
+// ------------------- Event Capturing----------------------
+
+// Event capturing - yaha event jumps from parent element to clicked child element but unlike bubbling we have to 
+// mannually enable event capturing by simply write ",true" after function
+// watch 3:55:00 of sheryian school js part 2 for better understanding
+
+
+// example of capturing
+
+// ul.addEventListener("click",val=>{
+//     // val.target.style.textDecoration = "line-through"
+//     val.target.classList.toggle("line-through")         // two way animation of line through with toggle
+// },true)
+
+
+// ----------------------- character left counter ----------------------
+
+let inp4 = document.querySelector(".character-counter input")
+let span = document.querySelector("span")
+inp4.addEventListener("input", () => {
+    // console.log(inp4.value.length)
+    let left = 20 - inp4.value.length
+    span.textContent = left
+    if (left < 0) {
+        span.style.color = "red"
+    }
+    else {
+        span.style.color = "white"
+    }
+})
